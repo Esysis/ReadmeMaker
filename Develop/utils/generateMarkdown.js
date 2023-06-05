45d06e88e15d5520393fb1ef69718835d2b0d70e
Yuga <Trev@brennberry.com>
2023-06-05 18:44:02 -0400

    completed markdown code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,3 +18,17 @@ function renderLicenseLink(license) {
   }
   return '';
 }
+
+// Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return (
+      `## License
+      
+This project is licensed under the ${license} license.`
+    );
+  }
+  return '';
+}
+
