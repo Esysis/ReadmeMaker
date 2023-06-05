bbee7837471fdfa888ebaf8aa6b1b5ae6dc0344f
Yuga <Trev@brennberry.com>
2023-06-05 18:28:01 -0400

    Added my code to index

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,14 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+// Create an array of questions for user input
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    // Add more questions here...
+];
