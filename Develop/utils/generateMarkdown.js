7e9997fbdb824c187b42dabc3d0a19375625fb9f
Yuga <Trev@brennberry.com>
2023-06-05 18:41:55 -0400

    Added code to markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,20 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  }
+  return '';
 }
 
-module.exports = generateMarkdown;
+// Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return (
+      `\n* [License](#license)\n`
+    );
+  }
+  return '';
+}
