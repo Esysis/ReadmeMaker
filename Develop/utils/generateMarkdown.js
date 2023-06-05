e72327f890d1e94d8177e030c21c2562f7092472
Yuga <Trev@brennberry.com>
2023-06-05 18:47:23 -0400

    Actually finished the markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -32,3 +32,47 @@ This project is licensed under the ${license} license.`
   return '';
 }
 
+
+// Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return (
+      `## License
+      
+This project is licensed under the ${license} license.`
+    );
+  }
+  return '';
+}
+
+// Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description 
+
+  ${data.description}
+
+  ## Table of Contents 
+
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  * [Contributing](#contributing)
+  
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  // add more sections as required
+
+  ${renderLicenseSection(data.license)}
+  `;
+}
+
+module.exports = generateMarkdown;
+
+
