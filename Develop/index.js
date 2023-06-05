5688745540fb2c224c7f630e2dd62046403b6da5
Yuga <Trev@brennberry.com>
2023-06-05 18:36:42 -0400

    Added last of index code

--- Develop/index.js
+++ Develop/index.js
@@ -12,3 +12,27 @@ const questions = [
     },
     // Add more questions here...
 ];
+
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success! Your README.md file has been created!")
+    });
+}
+
+// Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            // Pass inquirer data to generateMarkdown function
+            writeToFile("README.md", generateMarkdown(answers));
+        })
+}
+
+// Function call to initialize app
+init();
+
